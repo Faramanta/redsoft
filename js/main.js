fa4c54cad03b364f134e73b355a0f3c40cb96d14
Faramanta <katuxa@yandex.ru>
2018-03-16 00:21:17 +0500

    left filter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $('select[name="hotelType"]').SumoSelect();
     $('select[name="hotel"]').SumoSelect();
+    $('select[name="areal"]').SumoSelect();
+    $('select[name="meal"]').SumoSelect();
+    $('select[name="services"]').SumoSelect();
+    $('select[name="landmarks"]').SumoSelect();
 });
